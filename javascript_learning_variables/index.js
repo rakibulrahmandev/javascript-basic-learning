df60a4a5d18d977b91ada818dcd68584d6a8eb8b
devrakibulrahman <devrakibulrahman@gmail.com>
2025-03-09 16:50:07 +0600

    javascript variable practice done. #javascript_variable

---
+++ javascript_learning_variables/index.js
@@ -0,0 +1,36 @@
+//? javascript variable practice problems ----------------------------------------------------------------------------------->
+
+//! #problem-1 ------------------------------------------------------------------------------------------>
+//* Put your birth year in a variable.
+
+const birthYear = 2006;
+
+//! #problem-2 ------------------------------------------------------------------------------------------>
+//* How many brothers and sisters do you have, put that in a variable.
+
+const brotherAndSister = 0;
+
+//! #problem-3 ------------------------------------------------------------------------------------------>
+//* Declare a variable called students with the number of students in your class.
+
+const students = 50;
+
+//! #problem-4 ------------------------------------------------------------------------------------------>
+//* Declare a variable called marks with the marks you got in the last exam.
+
+const marks = 84;
+
+//! #problem-5 ------------------------------------------------------------------------------------------>
+//* Declare a variable called rooms to indicate how many rooms there are in your house.
+
+const rooms = 1;
+
+//! #problem-6 ------------------------------------------------------------------------------------------>
+//* Keep your daily study time in hours in a variable.
+
+const studyTimes = 10;
+
+//! #problem-7 ------------------------------------------------------------------------------------------>
+//* Add up all the digits in your phone number and put the sum in a variable called Total.
+
+const total = 46;
