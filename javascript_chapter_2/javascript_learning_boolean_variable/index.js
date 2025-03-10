5094555128626e58692553233fd1854e1d16f0ae
devrakibulrahman <devrakibulrahman@gmail.com>
2025-03-10 20:54:03 +0600

    javascript boolean variable practice done. #javascript_boolean_variable

---
+++ javascript_chapter_2/javascript_learning_boolean_variable/index.js
@@ -0,0 +1,48 @@
+//? javascript true & false practice problems --------------------------------------------------------------------------------------------->
+
+//! #problem-1 -------------------------------------------------------------------------------------------------->
+//* Suppose your friend is eating bamboo in the exam. Now declare a boolean type variable to determine whether he "isHappy" or not and store its value. And print it to the console.
+
+const isBamboo = true;
+console.log(isBamboo);
+
+//! #problem-2 -------------------------------------------------------------------------------------------------->
+//* Write a program yourself to check if your crush has sent you a message or not. If the message is sent, return true, if not, return false. Print it to the console.
+
+const isCrushReplied = false;
+console.log(isCrushReplied);
+
+//! #problem-3 -------------------------------------------------------------------------------------------------->
+//* Your favorite team won the game, now to celebrate, declare a boolean variable called "celebrating", give it a value, and see the output.
+
+const celebrating = true;
+console.log(celebrating);
+
+//! #problem-4 -------------------------------------------------------------------------------------------------->
+//* You now have a good idea about boolean types. Now write a program where you declare three boolean variables, "isHappy", "hasMoney", and "isHungry". Set these three values ​​to true, false, and true respectively. Then output them to the console.
+
+const isHappy = true;
+const hasMoney = false;
+const isHungry = false;
+
+console.log(isHappy);
+console.log(hasMoney);
+console.log(isHungry);
+
+//! #problem-5 -------------------------------------------------------------------------------------------------->
+//* Have you finished studying today? If you finished studying, it will be true, otherwise it will be false. And to store that, declare a boolean variable called "isStudyDone".
+
+const isStudyDone = false;
+console.log(isStudyDone);
+
+//! #problem-6 -------------------------------------------------------------------------------------------------->
+//* Now declare a boolean variable called "isPhoneCharged". True if charging is complete, false otherwise.
+
+const isPhoneCharged = true;
+console.log(isPhoneCharged);
+
+//! #problem-7 -------------------------------------------------------------------------------------------------->
+//* Have you finished dinner? Declare a boolean variable called "isDinnerDone" for that. You already know what to write if you're done eating, and what to write if you're not done eating.
+
+const isDinnerDone = false;
+console.log(isDinnerDone);
