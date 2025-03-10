d9dc497d4cd8e25067ffa8bc25c01e9b29009da5
devrakibulrahman <devrakibulrahman@gmail.com>
2025-03-10 20:15:30 +0600

    javascript string practice done. #javascript_string

---
+++ javascript_chapter_2/javascript_learning_string/index.js
@@ -0,0 +1,43 @@
+//? javascript string (string declare) practice problem ---------------------------------------------------------------------------------------------->
+
+//! #problem-1 --------------------------------------------------------------------------------------------------------------->
+//* Now declare a variable called "name", whose value will be your "name". Then display it as output in the console.
+
+const name = "Rakibul Rahman";
+console.log(name);
+
+//! #problem-2 --------------------------------------------------------------------------------------------------------------->
+//* Declare your "address" as a string variable and use the console to see the output.
+
+const address = 'Dhaka, Bangladesh';
+console.log(address);
+
+//! #problem-3 --------------------------------------------------------------------------------------------------------------->
+//* Declare a variable with the name of the school, college or university your best friend attends. Then display it as output.
+
+const college = "Daffodil International College";
+console.log(college);
+
+//! #problem-4 --------------------------------------------------------------------------------------------------------------->
+//* Put the name of your favorite heroine in a variable called "fan". Then view it in the console.
+
+const fan = 'Esma Rahman';
+console.log(fan);
+
+//! #problem-5 --------------------------------------------------------------------------------------------------------------->
+//* Put the name of your favorite movie of this year in a variable called "movieOfTheYear". Then print it to the console.
+
+const movieOfTheYear = "Mad Max";
+console.log(movieOfTheYear);
+
+//! #problem-6 --------------------------------------------------------------------------------------------------------------->
+//* Everyone has some "hobby". Declare a string variable called your "hobby".
+
+const hobby = 'Traveling';
+console.log(hobby);
+
+//! #problem-7 --------------------------------------------------------------------------------------------------------------->
+//* What is your favorite "color"? Declare a string variable called "color".
+
+const color = 'Black';
+console.log(color);
