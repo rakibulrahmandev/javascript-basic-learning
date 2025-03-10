e68981a474a235232961e208712e1b9e05bbb44c
devrakibulrahman <devrakibulrahman@gmail.com>
2025-03-10 17:45:24 +0600

    javascript console (print program) practice done. #javascript_console

---
+++ javascript_chapter_2/javascript_learning_console/index.js
@@ -0,0 +1,50 @@
+//? javascript console (print the code in console) practice problems --------------------------------------------------------------------------->
+
+//! #problem-1 ------------------------------------------------------------------------------------------------>
+//* Keep a variable called "age" that contains your current age. Then print this variable to the console.
+
+const age = 18;
+
+console.log(age);
+
+//! #problem-2 ------------------------------------------------------------------------------------------------>
+//* Put the number of books you plan to read this year in a variable called "booksToRead". Then, output the value of "booksToRead" to the console.
+
+const booksToRead = 4;
+
+console.log(booksToRead);
+
+//! #problem-3 ------------------------------------------------------------------------------------------------>
+//* Declare a variable called "wakeUpTime" to know what time you wake up every morning. Then set the new value of "wakeUpTime" and see the output in the console.
+
+const wakeUpTime = 5;
+
+console.log(wakeUpTime);
+
+//! #problem-4 ------------------------------------------------------------------------------------------------>
+//* How many hours of time have you wasted on your mobile phone today? Put this value in a variable called "wastedTime". Then view it in the console.
+
+const wastedTime = 2;
+
+console.log(wastedTime);
+
+//! #problem-5 ------------------------------------------------------------------------------------------------>
+//* How much money are you borrowing right now? Put that in a variable called "borrow".
+
+const borrow = 20;
+
+console.log(borrow);
+
+//! #problem-6 ------------------------------------------------------------------------------------------------>
+//* Find the maximum number of goals your favorite team has conceded to the opposing team in a match. Write that in a variable called "goals" and display it as output.
+
+const goals = 2;
+
+console.log(goals);
+
+//! #problem-7 ------------------------------------------------------------------------------------------------>
+//* Declare a variable called "pillow" to count how many pillows you have on your bed. Then display that as output in the console.
+
+const pillow = 3;
+
+console.log(pillow);
