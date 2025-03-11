fe1059fcc98cec97f92b97fbbee4422f9c9af680
devrakibulrahman <devrakibulrahman@gmail.com>
2025-03-11 12:42:32 +0600

    javascript data type practice done. #javascript_data_type

---
+++ javascript_chapter_3/javascript_data_type/index.js
@@ -0,0 +1,38 @@
+//? javascript data type practice problems --------------------------------------------------------------------------------------------------------->
+
+//! #problem-1 ------------------------------------------------------------------------------------------------------>
+//* Write a program using "typeof" to check the type of a variable. For example, declare a variable "price" with a value of 555, now check the type in the console.
+
+const price = 555;
+console.log(typeof price); // output: number;
+
+//! #problem-2 ------------------------------------------------------------------------------------------------------>
+//* Declare a variable "learning" with the value true, and output it to the console. Then use "typeof" to see what type of variable it is.
+
+const learning = true;
+console.log(learning); // output: true;
+console.log(typeof learning); // output: boolean;
+
+//! #problem-3 ------------------------------------------------------------------------------------------------------>
+//* A variable called "megaCity" has the value "Dhaka". Now use "typeof" to see what type this variable is.
+
+const megaCity = "Dhaka";
+console.log(typeof megaCity); // output: string;
+
+//! #problem-4 ------------------------------------------------------------------------------------------------------>
+//* You put your hand on your forehead in the scorching heat, and you felt like you were about to burn yourself. At that moment, you declared a variable called "temperature", whose value is 102.5. Check what type of variable it is with "typeof".
+
+const temperature = 102.5;
+console.log(typeof temperature); // output: number;
+
+//! #problem-5 ------------------------------------------------------------------------------------------------------>
+//* You create a variable called "isHungry" because you are hungry, and its value is true. Now use console and "typeof" to check its type.
+
+const isHungry = true;
+console.log(typeof isHungry); // output: boolean;
+
+//! #problem-6 ------------------------------------------------------------------------------------------------------>
+//* There is a variable named "isRich", whose value is true. Now check its type in the console.
+
+const isRich = true;
+console.log(typeof isRich); // output: boolean;
