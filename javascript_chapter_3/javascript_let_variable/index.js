1365e985b3cd72a1ee4c3331934d7acaf757219f
devrakibulrahman <devrakibulrahman@gmail.com>
2025-03-11 16:54:43 +0600

    javascript let variable practice done. #javascript_let_variable

---
+++ javascript_chapter_3/javascript_let_variable/index.js
@@ -0,0 +1,67 @@
+//? javascript let variable practice problems -------------------------------------------------------------------------------------->
+
+//! #problem-1 ------------------------------------------------------------------------------------>
+//* Put your current age in a variable called "age". Print it to the console. Then set the new value in the "age" variable and print it to the console again.
+
+let age = 18;
+console.log(age); // output: 18;
+
+age = 15;
+console.log(age); // output: 15;
+
+//! #problem-2 ------------------------------------------------------------------------------------>
+//* You now have 500 taka in your pocket, put it in a variable called "pocketMoney". Then set the value of "pocketMoney" to 150 and print it to the console.
+
+let pocketMoney = 500;
+console.log(pocketMoney); // output: 500;
+
+pocketMoney = 150;
+console.log(pocketMoney); // output: 150;
+
+//! #problem-3 ------------------------------------------------------------------------------------>
+//* You plan to study for 4 hours, create a variable called "studyTime". Then update the value of "studyTime" to the number of hours you actually studied today. If the value is zero, set it to 0 and check the console.
+
+let studyTime = 4;
+console.log(studyTime); // output: 4;
+
+studyTime = 1;
+console.log(studyTime); // output: 1;
+
+//! #problem-4 ------------------------------------------------------------------------------------>
+//* You have a total of 3 friends, put this number in a variable called "friendsCount". Later, 2 of your friends left you. Now update the value of "friendsCount" and see its output in the console.
+
+let friendsCount = 3;
+console.log(friendsCount); // output: 3;
+
+friendsCount = 1;
+console.log(friendsCount); // output: 1;
+
+//! #problem-5 ------------------------------------------------------------------------------------>
+//* You bought a new phone for 25000 taka. Declare a variable with that. And after a month, when you go to sell that phone, you see that the price is only 12000. What else can you do? First, give the value of a variable named "phonePrice" 25000. Then update it with 12000 and see it in the console.
+
+let phonePrice = 25000;
+console.log(phonePrice); // output: 25000;
+
+phonePrice = 12000;
+console.log(phonePrice); // output: 12000;
+
+//! #problem-6 ------------------------------------------------------------------------------------>
+//* There are 40 chairs in your class. After a few days, the legs of 2 chairs broke. Update this value in a variable called "chairsCount". After a few more days, the seniors from the next room came and took 6 chairs. This time also update the value of "chairsCount" and output the final value to the console.
+
+let chairsCount = 40;
+console.log(chairsCount); // output: 40;
+
+chairsCount = chairsCount - 2;
+console.log(chairsCount); // output: 38;
+
+chairsCount = chairsCount - 6;
+console.log(chairsCount); // output: 32;
+
+//! #problem-7 ------------------------------------------------------------------------------------>
+//* You plan to play for 3 hours today, put it in a variable called "playTime". Then update the value of "playTime" to reflect how many hours you actually played. If you didn't play, set the value of "playTime" to 0 and check the console.
+
+let playTime = 3;
+console.log(playTime); // output: 3;
+
+playTime = 0;
+console.log(playTime); // output: 0;
