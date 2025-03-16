4ae1c55dc33032412e94fbd7b0f6ebec7f93e092
devrakibulrahman <devrakibulrahman@gmail.com>
2025-03-16 12:16:39 +0600

    javascript variable comparison practice done. #javascript_variable_comparison

---
+++ javascript_chapter_4/javascript_variable_comparison/index.js
@@ -0,0 +1,65 @@
+//? javascript variable comparison practice problems --------------------------------------------------------------------------->
+
+//! #problem-1 ----------------------------------------------------------------------------->
+//* Your younger brother is 10 years old, and you are 15 years old. Write a program to check whether your younger brother's age is less than or equal to your age.
+
+let youngerBrotherAge = 10;
+let myAge = 15;
+
+console.log(youngerBrotherAge <= myAge); // output: true; because my younger brother's age is less than mine.
+
+//! #problem-2 ----------------------------------------------------------------------------->
+//* The passing marks in an exam are 33. Suppose you got 45. Check whether you passed or not.
+
+let passingMarks = 33;
+let iGotMarks = 45;
+
+console.log(iGotMarks >= passingMarks); // output: true; because I got marks greater than or equal to the passing marks.
+
+//! #problem-3 ----------------------------------------------------------------------------->
+//* The two variables are the prices of grapefruit and kangaroo. Compare whether grapefruit is cheaper than kangaroo.
+
+let grapefruitPrice = 50;
+let kangarooPrice = 80;
+
+console.log(grapefruitPrice < kangarooPrice); // output: true; because grapefruit is cheaper than kangaroo.
+
+//! #problem-4 ----------------------------------------------------------------------------->
+//* You have read 10 books, and your friend is reading 10. Check whether you have read the same number of books or not.
+
+let iReadBooks = 10;
+let friReadBooks = 10;
+
+console.log(iReadBooks == friReadBooks); // output: true;
+
+//! #problem-5 ----------------------------------------------------------------------------->
+//* You have 15 biscuits and your friend has 20. Check whether you got fewer biscuits than your friend.
+
+let iHaveBiscuits = 15;
+let friHasBiscuits = 20;
+
+console.log(iHaveBiscuits < friHasBiscuits); // output: false; because I have fewer biscuits than my friend.
+
+//! #problem-6 ----------------------------------------------------------------------------->
+//* In a football match, Real Madrid scores 2 goals, and Barca scores 3 goals. Check whether Real Madrid has scored fewer goals than Barca.
+
+let realMadridGoals = 2;
+let barcaGoals = 3;
+
+console.log(realMadridGoals < barcaGoals); // output: true; because 2 is less than 3.
+
+//! #problem-7 ----------------------------------------------------------------------------->
+//* Your name is "Rahim", and your friend's name is "Karim". Check if your name is the same as your friend's name.
+
+let myName = 'Rahim';
+let myFriendName = 'Karim';
+
+console.log(myName == myFriendName); // output: false; because which are not the same.
+
+//! #problem-7 ----------------------------------------------------------------------------->
+//* You studied today (true), and your friend did not study (false). Check, did you do the same thing?
+
+let iStudyToday = true;
+let friStudy = false;
+
+console.log(iStudyToday == friStudy); // output: false; because true is not equal to false.
